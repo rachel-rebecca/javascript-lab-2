9c817cdb6e449a3215300320baea359c250f8ddf
rachel-rebecca <Rarebecc@umich.edu>
2021-06-15 13:21:44 -0400

    attempt to add in my code

--- src/script.js
+++ src/script.js
@@ -1,3 +1,54 @@
 "use strict";
 
 // TODO - write your code here.
+
+const randomDamage = () => {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+const chooseOption  = (opt1, opt2) => {
+    let randNum = Math.round(Math.random()) === 0? opt1: opt2;
+    return randNum;
+}
+
+const attackPlayer = function (health) {
+    return health - randomDamage();
+}
+
+const logHealth = (player, health) => {
+    console.log(`${player} health: ${health}`);
+    
+}
+
+const logDeath = (winner, loser) => {
+    console.log(`${winner} defeated ${loser}`);
+}
+
+const isDead = health =>  health <= 0;
+
+
+function fight(player1, player2, player1Health, player2Health) {
+    while (true) {
+        let attacker = chooseOption(player1, player2);
+        if (attacker === player1) {
+            player2Health = attackPlayer(player2Health);
+            logHealth(player2, player2Health);
+            if (isDead(player2Health) === true) {
+                logDeath(player1, player2);
+                break;
+            }
+            
+        } else {
+            player1Health = attackPlayer(player1Health);
+            logHealth(player1, player1Health);
+            if (isDead(player1Health) === true) {
+                logDeath(player2, player1);
+                break;
+            }
+            
+        }
+    }
+    
+} 
+
+console.log(fight("Mario", "Bowser", 100, 100));
